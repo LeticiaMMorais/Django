ae88c0b12569a2cc25a476496dd7a10df632492c
Letícia Morais <leticialeticiamaria674@gmail.com>
2026-06-12 20:22:28 -0300

    Retirando algumas variáveis desnecessárias que esqueci em contexto.py na atividade 'doceria'

--- PI/e2/doceria/contexto.py
+++ PI/e2/doceria/contexto.py
@@ -13,27 +13,10 @@ def return_context():
         {'nome':'Pudim', 'descricao':'Pudim feito com leite condensado. É lisinho e delicioso. O tamanho é médio.', 'preco':32.00},
         {'nome':'Bolo de Rolo', 'descricao':'Bolo de rolo com recheio de goiabada e finalização com açúcar de confeiteiro.', 'preco':30.00},
     ]
-    descricao_produtos = {
-            'Brownie':'Brownie macio por dentro e com casquinha por fora. São duas camadas de tamanho 5cmx5cm com recheio de ninho', 
-            'Red Velvet':'',
-            'Bolo de Abacaxi':'',
-            'Pudim':'',
-            'Bolo de Rolo':''
-        }
-    preco_produtos = {
-            'Brownie':7.00,
-            'Red Velvet':35.00,
-            'Bolo de Abacaxi':52.00,
-            'Pudim':32.00,
-            'Bolo de Rolo':30.00,
-        }
 
     return {
         'nome_loja':nome_loja,
         'ano_fundacao':fundacao,
         'servicos':servicos,
         'produtos':produtos,
-        'descricao_produtos':descricao_produtos,
-        'preco_produtos': preco_produtos
-
     }
